ae9276df7b7daa176d8d6639b08b6519ac42d25f
Taylor Monacelli <taylormonacelli@gmail.com>
2023-04-25 21:51:03 -0700

    real men dont make git commit comments

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-import os
-import boto3
 import json
+import os
 
-sns = boto3.client('sns', region_name='us-west-2')
+import boto3
 
-topic_arn = os.environ.get('SNS_TOPIC_ARN')
+sns = boto3.client("sns", region_name="us-west-2")
+
+topic_arn = os.environ.get("SNS_TOPIC_ARN")
 if not topic_arn:
-    print('Missing SNS_TOPIC_ARN environment variable')
+    print("Missing SNS_TOPIC_ARN environment variable")
     exit(1)
 
-message = {
-    'default': 'test message'
-}
+message = {"default": "test message"}
 
 try:
     response = sns.publish(
         TopicArn=topic_arn,
-        Message=json.dumps({'default': json.dumps(message)}),
-        MessageStructure='json'
+        Message=json.dumps({"default": json.dumps(message)}),
+        MessageStructure="json",
     )
     print(f"Message published with ID: {response['MessageId']}")
 except Exception as e:
     print(f"Failed to publish message: {e}")
+
+
+print(response["MessageId"]["HTTPSStatusCode"])
