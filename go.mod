b9feeccca5298f05a82d6af28e78d1d2fa9cca70
Taylor Monacelli <taylormonacelli@gmail.com>
2023-04-25 19:34:34 -0700

    Initial

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/taylormonacelli/quickturn
+
+go 1.20
+
+require github.com/aws/aws-sdk-go v1.44.250
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "fmt"
+
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/sns"
+)
+
+func main() {
+    // Set up AWS credentials
+    sess, err := session.NewSession(&aws.Config{
+        Region: aws.String("YOUR_REGION"),
+    })
+
+    if err != nil {
+        fmt.Println("Failed to create AWS session", err)
+        return
+    }
+
+    // Connect to the SNS service
+    svc := sns.New(sess)
+
+    // Set up the SNS topic ARN to publish to
+    topicArn := "YOUR_TOPIC_ARN"
+
+    // Publish the message to the topic
+    _, err = svc.Publish(&sns.PublishInput{
+        Message:  aws.String("hello"),
+        TopicArn: aws.String(topicArn),
+    })
+
+    if err != nil {
+        fmt.Println("Failed to publish message to SNS", err)
+        return
+    }
+
+    fmt.Println("Message published to SNS")
+}
