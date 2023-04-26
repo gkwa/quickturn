f180bd1918c770a8df03a735fa2cff381959113d
Taylor Monacelli <taylormonacelli@gmail.com>
2023-04-25 22:12:32 -0700

    real men dont make git commit comments

---
+++ t2/README.md
@@ -0,0 +1,1 @@
+# https://medium.com/analytics-vidhya/create-notifications-in-minutes-with-amazon-sns-and-python-6576961db40c
--- t2/main2.py
+++ t2/main2.py
@@ -1,3 +1,5 @@
+# https://medium.com/analytics-vidhya/create-notifications-in-minutes-with-amazon-sns-and-python-6576961db40c
+
 import logging
 import time
 
